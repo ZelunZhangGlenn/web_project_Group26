da12d2e4f1fa2e3843b7db2cc95260e1d83c61f6
ZelunZhangGlenn <a1801544@student.adelaide.edu.au>
2021-05-14 09:15:43 +0000

    finish millstone

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -23,15 +23,84 @@ function gototop(){
 }
 
 function function_on_click_home(){
-    // var main = document.getElementById("main");
-    // var mainPage = document.getElementById("mainPage");
-    // main.innerHTML = "";
-    // mainPage.style.display = "block";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var mainPart = document.getElementById("main");
+            var homePage = document.getElementById("homePage");
+            var news = document.getElementById("news_specfic_1");
+            var vaccine = document.getElementById("vaccination");
+            var banner = document.getElementById("banner");
+            homePage.style.display = "block";
+            news.style.display = "none";
+            vaccine.style.display = "none";
+            banner.style.display = "none";
+        }
+    };
+    xhttp.open("get", "/home", true);
+    xhttp.send();
 
 }
 
+function function_on_click_news_overall(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var mainPart = document.getElementById("main");
+            var homePage = document.getElementById("homePage");
+            var news = document.getElementById("news_specfic_1");
+            var vaccine = document.getElementById("vaccination");
+            var banner = document.getElementById("banner");
+            homePage.style.display = "none";
+            news.style.display = "block";
+            vaccine.style.display = "none";
+            banner.style.display = "none";
+        }
+    };
+    xhttp.open("get", "/news", true);
+    xhttp.send();
 
+}
 
+function function_on_click_WhatisCovid(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var mainPart = document.getElementById("main");
+            var homePage = document.getElementById("homePage");
+            var news = document.getElementById("news_specfic_1");
+            var vaccine = document.getElementById("vaccination");
+            var banner = document.getElementById("banner");
+            homePage.style.display = "none";
+            news.style.display = "none";
+            vaccine.style.display = "block";
+            banner.style.display = "none";
+        }
+    };
+    xhttp.open("get", "/HealthTopics/WhatisCovid", true);
+    xhttp.send();
+
+}
+
+function function_on_click_Vaccine(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var mainPart = document.getElementById("main");
+            var homePage = document.getElementById("homePage");
+            var news = document.getElementById("news_specfic_1");
+            var vaccine = document.getElementById("vaccination");
+            var banner = document.getElementById("banner");
+            homePage.style.display = "none";
+            news.style.display = "none";
+            vaccine.style.display = "none";
+            banner.style.display = "block";
+        }
+    };
+    xhttp.open("get", "/HealthTopics/Vaccine", true);
+    xhttp.send();
+
+}
 
 
 // Head bar //
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/home', function(req, res, next)) {
-    res.send(
-        
-        `
-            
-        `    
-        
-    );
-}
+router.get('/home', function(req, res, next) {
+    res.send();
+});
+
+router.get('/news', function(req, res, next) {
+    res.send();
+});
+
+router.get('/HealthTopics/WhatisCovid', function(req, res, next) {
+  res.send();
+});
+
+router.get('/HealthTopics/Vaccine', function(req, res, next) {
+  res.send();
+});
 
 module.exports = router;
